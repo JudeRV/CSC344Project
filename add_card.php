ca81db1a6f88d1e51d889e03e96a59c5a8f405a7
Jude Vargas <judevargas22@gmail.com>
2024-11-29 03:26:52 -0500

    Progress on card search page
    - Changed add_card.html to add_card.php
    - Got user-submitted info to server side for processing
    - Started work on plugging it into the API
    - PROBLEM: API only accepts HTTPS requests so we need an SSL certfification :( I'll handle it don't worry

---
+++ add_card.php
@@ -0,0 +1,56 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	if (isset($_POST["card_name"])) {
+		$cardName = htmlspecialchars($_POST["card_name"]);
+	}
+	else {
+		echo("You must provide a card name.");
+		return;
+	}
+	/*
+		https://scryfall.com/docs/api
+	*/
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://api.scryfall.com/cards/named?fuzzy=" . $cardName); // TODO: configure this url properly
+	curl_Setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return data instead of displaying it
+	curl_setopt($ch, CURLOPT_HTTPHEADER, [
+		"User-Agent: MTGDatabaseApp/1.0",
+		"Accept: application/json"
+	]);
+
+	$response = curl_exec($ch); // PROBLEM: API only accepts HTTPS requests, so we need to get an SSL cert :( I'll handle it
+
+	if ($response === false) {
+		echo("Error fetching API data: " . curl_error($ch));
+	}
+	else {
+		$data = json_decode($respose, true, );
+		echo($data);
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Add a New Card</title>
+	<meta charset ="utf-8"> 
+	<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+	<style>
+		label {display: block; float:left; width:10%;}
+		input {display: block; width: 20%}
+		#submit {width: 10%; margin-top: 2rem; margin-left: 2%;}
+	</style>
+</head>
+<body>
+	<h2>Add a New Card:</h2>
+	
+	<form action="add_card.php" method="POST">
+		<input type="text" name="card_name" value="Card Name" id = "card_name_text">
+        <input type="submit" name="submit" value="Search Cards" id = "submit">
+	</form>
+
+    <script>
+
+    </script>
+</body>
+</html>
